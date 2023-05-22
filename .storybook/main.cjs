da940510c5edd6e1e0aebef7be4e28776554b6b9
CrispyBacon1999 <bacon.josh09@gmail.com>
2023-05-22 16:45:50 -0400

    docs(storybook): update storybook version from 6.x to 7.x. This changes quite a lot of stuff with it

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-    stories: [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    ],
-    addons: [
-        "@storybook/addon-essentials",
-        {
-            name: "@storybook/addon-docs",
-            options: { configureJSX: true },
-        },
-    ],
-    core: {
-        builder: "@storybook/builder-vite",
-    },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials", {
+    name: "@storybook/addon-docs",
+    options: {
+      configureJSX: true
+    }
+  }],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
